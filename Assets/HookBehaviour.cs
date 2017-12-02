b08c70bfbb950a93a88c6b18b19aaa44b699f2b0
Tinco Andringa <mail@tinco.nl>
2017-12-02 18:49:55 +0100

    add hookbehaviour and pickup behaviour

---
+++ Assets/HookBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookBehaviour : MonoBehaviour {
+	public List<Pickupable> AttachedPickups;
+
+	public float BreakForce = 100;
+	public float BreakTorque = 100;
+	public float JointDamper = 1;
+	public float JointForce = 1;
+
+	void OnCollisionEnter(Collision collision)
+	{
+		foreach (ContactPoint contact in collision.contacts)
+		{
+			var otherBody = contact.otherCollider.attachedRigidbody;
+			var pickupable = otherBody ? otherBody.gameObject.GetComponent<Pickupable> () : null;
+			if (pickupable != null && !AttachedPickups.Contains(pickupable)) {
+				Attach (pickupable, contact);
+			}
+		}
+	}
+
+	void Attach(Pickupable pickup, ContactPoint contact) {
+		AttachedPickups.Add (pickup);
+		CreatePickupJoint (pickup.gameObject, contact);
+		pickup.Attach (this);
+	}
+
+	// This method is called by the pickup when the joint breaks.
+	public void Detach(Pickupable pickup) {
+		Debug.Log ("Pickup detached!");
+		AttachedPickups.Remove (pickup);
+	}
+
+	void CreatePickupJoint(GameObject pickup, ContactPoint contact) {
+		var joint = pickup.AddComponent<SpringJoint> ();
+		joint.connectedBody = gameObject.GetComponent<Rigidbody> ();
+		joint.breakForce = BreakForce;
+		joint.breakTorque = BreakTorque;
+		joint.enableCollision = true;
+		joint.damper = JointDamper;
+		joint.spring = JointForce;
+	}
+}
---
+++ Assets/Pickupable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickupable : MonoBehaviour {
+	// This is set when this pickup is hooked by a player
+	public HookBehaviour hook;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// This is called when we're hooked by a player
+	public void Attach(HookBehaviour hook) {
+		this.hook = hook;
+	}
+
+	// This class is responsible for telling the hooking player that the joint broke.
+	void OnJointBreak(float breakForce)
+	{
+		this.hook.Detach (this);
+	}
+}
