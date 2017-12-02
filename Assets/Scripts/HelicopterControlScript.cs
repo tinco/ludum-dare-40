876e54d9c489fee88c54ddf13680811db4fbe5cf
Ivan Mous <bificommand@gmail.com>
2017-12-02 13:54:38 +0100

    Initial Project

---
+++ Assets/Scripts/HelicopterControlScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelicopterControlScript : MonoBehaviour {
+
+
+    private float baseThrust = 50000;
+    private float inputThrust = 40000;
+    private float gravityForce = 1000;
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float currentThrust = baseThrust + Input.GetAxis("Vertical") * inputThrust;
+        rb.AddRelativeForce((
+                Vector3.up * currentThrust 
+                //- Vector3.up * rb.velocity.y * 0.01f
+                ) 
+            * Time.deltaTime);
+        //rb.AddForce(-Vector3.up * gravityForce * Time.deltaTime);
+	}
+}
