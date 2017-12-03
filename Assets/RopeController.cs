5fe292d601832096ef70e1344c1ae1647eef7859
Tinco Andringa <mail@tinco.nl>
2017-12-03 18:29:29 +0100

    move some files and implement rendering of hook rope

---
+++ Assets/RopeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode()]
+public class RopeController : MonoBehaviour {
+	public List<Transform> Positions;
+
+	public LineRenderer LineRenderer;
+	
+	// Update is called once per frame
+	void Update () {
+		LineRenderer.positionCount = Positions.Count;
+		int i = 0;
+		foreach (var position in Positions) {
+			LineRenderer.SetPosition (i, position.position);
+			i += 1;
+		}
+	}
+}
