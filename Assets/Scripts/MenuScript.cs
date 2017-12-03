a2f56a7d068caf14a5bb14e34d454012643289cb
Ivan Mous <bificommand@gmail.com>
2017-12-03 18:41:06 +0100

    Menu toegevoegd

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+    public GameObject MainMenuPanel;
+    public GameObject LevelSelectPanel;
+
+    public void Start()
+    {
+        MainMenuPanel.SetActive(true);
+        LevelSelectPanel.SetActive(false);
+    }
+
+    public void StartNewGamePress()
+    {
+        MainMenuPanel.SetActive(false);
+        LevelSelectPanel.SetActive(true);
+    }
+
+    public void BackButtonPressed()
+    {
+        MainMenuPanel.SetActive(true);
+        LevelSelectPanel.SetActive(false);
+    }
+
+    public void QuitButtonPressed()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+
+    public void LevelPressed(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
