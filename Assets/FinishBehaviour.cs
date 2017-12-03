8df5d0af0f4107ac5d53f5c95e205cfad1dd3e0b
Tinco Andringa <mail@tinco.nl>
2017-12-03 16:46:04 +0100

    add finish

---
+++ Assets/FinishBehaviour.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishBehaviour : MonoBehaviour {
+	void OnTriggerEnter()
+	{
+		FindObjectOfType<GameController> ().BroadcastMessage("OnFinish");
+	}
+}
