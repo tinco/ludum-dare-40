5236f62290e216ed04239a7832d28a874927d726
Tinco Andringa <mail@tinco.nl>
2017-12-03 16:24:00 +0100

    dying works

---
+++ Assets/BreakBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakBehaviour : MonoBehaviour {
+	public GameObject Wreck;
+
+	void OnRotorCollision () {
+		FindObjectOfType<GameController> ().BroadcastMessage("OnDeath");
+		Instantiate(Wreck, transform.position, transform.rotation);
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public int Deaths = 0;
+	public GameObject Player;
+
+	void OnDeath() {
+		Deaths += 1;
+		var spawn = GameObject.FindGameObjectWithTag ("Respawn");
+		var playerInstance = Instantiate(Player, spawn.transform.position, spawn.transform.rotation);
+		var camera = GameObject.FindGameObjectWithTag ("MainCamera");
+		camera.GetComponent<CameraScript> ().Helicopter = playerInstance.GetComponent<Rigidbody>();
+	}
+}
