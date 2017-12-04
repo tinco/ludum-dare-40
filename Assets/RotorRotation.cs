00e66f1798f6ead97a1dcb4b4d6f058f40a24a45
Tinco Andringa <mail@tinco.nl>
2017-12-04 16:29:40 +0100

    rotate rotor

---
+++ Assets/RotorRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotorRotation : MonoBehaviour {
+	public enum OrientationType { Horizontal, Vertical }
+	public float RotationSpeed;
+	public OrientationType Orientation;
+
+	// Update is called once per frame
+	void Update () {
+		var orientationVector = Orientation == OrientationType.Horizontal ? new Vector3 (0, 0, 1) : new Vector3 (1, 0, 0);
+		gameObject.transform.Rotate (orientationVector * RotationSpeed);
+	}
+}
